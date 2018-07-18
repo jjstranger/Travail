7e71f3029e13fcd17ce95a4c306d09503da4c631
jjstranger <jjstranger@qq.com>
2018-07-19 00:32:20 +0800

    SynScrpt2Loc

---
+++ script/trv_synScriptToLocal.py
@@ -0,0 +1,30 @@
+import os
+from pymel import core as pmc
+
+def trv_synScrpt2Loc():
+	lsTrvScrptDir=os.listdir(travailRoot+’Script/’)
+	lsAllSrcScrpt=[item for item in lsTrvScrptDir if item.endswith(‘.mel’)]
+	lsAllSrcScrpt+=[item for item in lsTrvScrptDir if item.endswith(‘.py’)]
+
+	usrScrptDir=pmc.internalVar(usd=1)
+	lsDstScrptDir=os.listdir(usrScrptDir)
+	lsAllDstScrpt=[item for item in lsDstScrptDir if item.endswith(‘.mel’)]
+	lsAllDstScrpt+=[item for item in lsDstScrptDir if item.endswith(‘.py’)]
+
+	scrptToCopy=[]
+	pycDel=[]
+	for item in lsAllSrcScrpt:
+		if item in lsAllDstScrpt:
+			srcFileTCod=os.stat(travailRoot+’Script/‘+item).st_mtime
+			dstFileTCod=os.stat(usrScrptDir+item).st_mtime
+			if scrFileTCod!=dstFileTCod:
+				if os.path.exists(usrScrptDir+item+’c’):
+					pycDel.append(item)
+					print ‘[Delete] ‘+usrScrptDir+item+’c’
+				print ‘[Update] ‘+item
+				scrptToCopy.append(item)
+		else:
+			print ‘[New] ‘+item
+			scrptToCopy.append(item)
+	return scrptToCopy,pycDel
+#trv_synScrpt2Loc()
