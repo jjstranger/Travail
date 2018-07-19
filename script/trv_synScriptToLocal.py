7ba912b2bd4318efca29477a2e047607fb2bf1f3
jjs <jjstranger@qq.com>
2018-07-20 00:19:54 +0800

    refine synScr2Loc

--- script/trv_synScriptToLocal.py
+++ script/trv_synScriptToLocal.py
@@ -2,29 +2,27 @@ import os
 from pymel import core as pmc
 
 def trv_synScrpt2Loc():
-	lsTrvScrptDir=os.listdir(travailRoot+’Script/’)
-	lsAllSrcScrpt=[item for item in lsTrvScrptDir if item.endswith(‘.mel’)]
-	lsAllSrcScrpt+=[item for item in lsTrvScrptDir if item.endswith(‘.py’)]
+	lsTrvScrptDir=os.listdir(travailRoot+'/Script/')
+	lsAllSrcScrpt=[item for item in lsTrvScrptDir if item.endswith(('.mel','.py'))]
 
 	usrScrptDir=pmc.internalVar(usd=1)
 	lsDstScrptDir=os.listdir(usrScrptDir)
-	lsAllDstScrpt=[item for item in lsDstScrptDir if item.endswith(‘.mel’)]
-	lsAllDstScrpt+=[item for item in lsDstScrptDir if item.endswith(‘.py’)]
+	lsAllDstScrpt=[item for item in lsDstScrptDir if item.endswith(('.mel','.py'))]
 
 	scrptToCopy=[]
 	pycDel=[]
 	for item in lsAllSrcScrpt:
 		if item in lsAllDstScrpt:
-			srcFileTCod=os.stat(travailRoot+’Script/‘+item).st_mtime
+			srcFileTCod=os.stat(travailRoot+'/Script/'+item).st_mtime
 			dstFileTCod=os.stat(usrScrptDir+item).st_mtime
-			if scrFileTCod!=dstFileTCod:
-				if os.path.exists(usrScrptDir+item+’c’):
+			if srcFileTCod!=dstFileTCod:
+				if os.path.exists(usrScrptDir+item+'c'):
 					pycDel.append(item)
-					print ‘[Delete] ‘+usrScrptDir+item+’c’
-				print ‘[Update] ‘+item
+					print '[Delete] '+usrScrptDir+item+'c'
+				print '[Update] '+item
 				scrptToCopy.append(item)
 		else:
-			print ‘[New] ‘+item
+			print '[New] '+item
 			scrptToCopy.append(item)
 	return scrptToCopy,pycDel
 #trv_synScrpt2Loc()
