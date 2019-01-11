2fe73ba395ff22ada881ffb03d7640fb4f3506b2
jjstranger <jjstranger@qq.com>
2019-01-11 10:58:08 +0800

    js modify

--- script/jsFileTst.py
+++ script/jsFileTst.py
@@ -1,4 +1,6 @@
 import json as js
+
+#### write json file ####
 prjPath='d:/'
 pmc.select(hi=1)
 getSel=pmc.selected(tr=1)
@@ -7,10 +9,12 @@ jsFile=open(prjPath+'jsFileTst.json','w')
 jsdata={}
 for item in getSel:
     getObjNm=item
-    f[str(item)]={'assPath':'','type':'','parent':str(item.getParent()),'translate':list(item.getTranslation()),'rotation':list(item.getRotation()),'scale':list(item.getScale())}
+    getTMatrix=list(item.getMatrix()[0])+ list(item.getMatrix()[1])+ list(item.getMatrix()[2])+ list(item.getMatrix()[3])
+    f[str(item)]={'assPath':'','type':'','parent':str(item.getParent()),'tMatrix':getTMatrix}
 jsFile.write(js.dumps(jsdata,indent=4,separators=(', ',': ')))
 jsFile.close()
 
-loadJSFile=open()
+#### load json file ####
+loadJSFile=open('PATH')
 cphJSFile=js.load(loadJSFile)
 
