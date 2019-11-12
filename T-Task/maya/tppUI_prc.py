10edd1dea5d5caeaf507fbc320a40d5daeea4458
jjstranger <jjstranger@qq.com>
2019-11-13 00:34:14 +0800

    ttpui refine

--- T-Task/maya/tppUI_prc.py
+++ T-Task/maya/tppUI_prc.py
@@ -11,21 +11,27 @@ def tvcPrjPortalWinUI():
     
     pmc.rowLayout("lstPnl_rwLyt",ct2=("both","both"),cw2=(200,500),nc=2)
     pmc.columnLayout("prjFavLst_clmnLyt",p="lstPnl_rwLyt",co=("both",5))
-    pmc.separaror(st="in",h=10,w=200)
+    pmc.separator(st="in",h=10,w=200)
     pmc.textFieldButtonGrp("prjTitSrch_txFldBtnGrp",l="Projects:",bl="Search",p="prjFavLst_clmnLyt",cw3=(45,99,50),cl3=("left","both","right"))
     pmc.textScrollList(h=420,p="prjFavLst_clmnLyt",w=195)
     pmc.text(l="Collections:",p="prjFavLst_clmnLyt",h=20)
     pmc.textScrollList(h=120,p="prjFavLst_clmnLyt",w=195)
     
     pmc.columnLayout("rBrswClmnLyt",p="lstPnl_rwLyt",co=("left",5),h=600)
-    pmc.rowLayout("pathDscrpLn",p=rBswrClmnLyt",nc=5)
+    pmc.rowLayout("pathDscrpLn",p="rBrswClmnLyt",nc=5)
     pmc.text("dbRghtIcn",l=">>",w=20,p="pathDscrpLn")
-pmc.rowLayout("pathTrainCtrlRwLyt",p="pathDscrpLn",nc=20)
+    pmc.rowLayout("pathTrainCtrlRwLyt",p="pathDscrpLn",nc=20)
 
     for pathSect in ["3507_HJ_YLQQ","VFX","sequence","SC001","YLQQ_001_004","CG","scenes","effects"]:
-        pmc.button(l=pathSect,p="pathTrainCtrlRwLyt")
-        pmc.popuoMenu()
+        pmc.button(l=pathSect,p="pathTrainCtrlRwLyt",h=19)
+        pmc.popupMenu()
         pmc.menuItem()
+    
+    pmc.rowLayout("rBrwsrToolsRwLyt",p="rBrswClmnLyt",nc=4,w=500)
+    pmc.button("toAssetBtn",l="To assets",h=20,p="rBrwsrToolsRwLyt")
+    pmc.button("toSeqBtn",l="To shots",h=20,p="rBrwsrToolsRwLyt")
+    pmc.separator(st="none",w=100,p="rBrwsrToolsRwLyt")
+    pmc.textFieldButtonGrp("flSrchTxFldBtnGrp",bl="Search")
 
 tvcPrjPortalWinUI()    
 pmc.showWindow("prjPortalWinUI")
