03fcf23c5dc6820a74719a3b2231dff78f0008b1
jjstranger <jjstranger@qq.com>
2020-01-15 04:23:19 +0800

    rough pub of exprtToAbc

--- T-Task/maya/scripts/working/trv_exportToABC.py
+++ T-Task/maya/scripts/working/trv_exportToABC.py
@@ -1,40 +1,62 @@
 import pymel.core as pmc
 
-selGeoNameLs=[]
-def trv_filterObjsByName(selGeoNameLs):
-    geoLs=[]
-    for item in selGeoNameLs:
-        geoLs.extend(pmc.ls(item,r=1))
-    if len(geoLs)!=1:
-        pmc.confirmDialog(m="There are "+str(len(geoLs))+" objects fit the name '"+item+"'.")
-        pmc.select(geoLs)
-        return geoLs
+objFilterStringLs=[]
+def trv_filterObjsByName(objFilterStringLs):
+    filtedObjLs=[]
+    for item in objFilterStringLs:
+        filtedObjLs.extend(pmc.ls(item,r=1))
+    if len(filtedObjLs)!=1:
+        pmc.confirmDialog(m="There are "+str(len(filtedObjLs))+" objects fit the name '"+item+"'.")
+        pmc.select(filtedObjLs)
+    return filtedObjLs
 
-
-def trv_getFrameRange():
+def trv_getFrameRange():# refine to ani/static 2ways
     srartFrame=pmc.playbackOptions(q=1,min=1)
     endFrame=pmc.playbackOptions(q=1,max=1)
     return srartFrame,endFrame
 
-def trv_getExportFilePath():
-    filePath=pmc.workspace.getPath()+"/cache/Alembic/"
-    fileName="AbcExprtTest_v001.abc"
-    ExprtFilePath=filePath+fileName#pmc.workspace
-    print ExprtFilePath
+def trv_getExportFilePath(fileName):
+    filePath=pmc.workspace.getPath()+"/cache/Alembic/"# refine to workspace direction
+    ExprtFilePath=filePath+fileName
     return ExprtFilePath
 
 def getABCOPSets():
-    setsLs=pmc.ls("*AlembicOP",set=1,r=1)
-    return setsLs
+    setsLs=pmc.ls("*_AlembicOP*",set=1,r=1)
+    setsAssemble=[]
+    for item in setsLs:
+        getSetName=item.replace("_AlembicOP","")
+        assmb=[item,getSetName]
+        setsAssemble.append(assmb)
+    return setsAssemble
 
-def trv_exportToABCAction(task,exportfilePath,exportframerange):
-    
+def getJobLst():
+    try:
+        len(getABCOPSets())>0
+    except:
+        selLst=pmc.ls(sl=1)
+        try:
+            len(selLst)>0
+        except:
+            pmc.confirmDialog(m="No Alembic Export Job is found Also no object is selected for export.")
+        else:
+            return selLst
+    else: 
+        return getABCOPSets()
+
+def trv_exportToABCAction(exprtJobLs):
+    jobStr=[]
+    exportframerange=trv_getFrameRange()
     for task in exprtJobLs:
-        ExprtRootOption="-sn -uv -ws "
-        for obj in task:
-            ExprtRootOption+=("-rt "+obj+" ")
-        ExprtFrameRange=" -fr "+str(exportframerange[0]+" "+str(exportframerange[1])
-        ExprtFilePath=" -f "+
+        setMembr=pmc.sets(task[0],q=1)
+        ExprtRootOption=""
+        ExprtFlags="-sn -uv -ws -wfs "
+        for obj in setMembr:
+            objStr=pmc.ls(obj,r=1)[0].longName()
+            ExprtRootOption+=("-rt "+str(objStr)+" ")
+        ExprtFrameRange=" -fr "+str(exportframerange[0])+" "+str(exportframerange[1])
+        ExprtFilePath=" -f \""+trv_getExportFilePath(task[1])+".abc\""
+        jobStr.append(ExprtFlags+ExprtRootOption+ExprtFrameRange+str(ExprtFilePath))
 
-    jobCmd=""
-    pmc.AbcExport(v=1,j="-stripNamespace -uv -ws -fr 1 100-rt '|XX' -f 'PATH' ")
+    pmc.AbcExport(v=1,j=jobStr)
+
+trv_exportToABCAction(getJobLst())
