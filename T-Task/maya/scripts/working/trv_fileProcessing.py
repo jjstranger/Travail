43264540e9fdfb34fbbd3e8cb31835441c9cd159
jjstranger <jjstranger@qq.com>
2020-02-12 00:13:29 +0800

    refine fileProcs

--- T-Task/maya/scripts/working/trv_fileProcessing.py
+++ T-Task/maya/scripts/working/trv_fileProcessing.py
@@ -1,4 +1,3 @@
-import os
 testFile="C:/PRJ/dev/3001_JJS_PrjTst/VFX/sequences/SC001/SH_001/CG/scenes/Effects/abcOpSet_Test_jjs_v001.mb"
 
 def getFileVersionStr(fileSourcName,verNumPad=3):
@@ -6,21 +5,25 @@ def getFileVersionStr(fileSourcName,verNumPad=3):
     for pad in range(verNumPad):
         verRuleStr+="\d"
     import re
-    fileVerStr=re.search(verRuleStr,fileSourcName).group()[2:]
-    return fileVerStr
+    strSpltLs=re.split(verRuleStr,fileSourceName)
+    if len(strSpltLs)==2:
+        fileVerStr=re.search(verRuleStr,fileSourcName).group()[2:]
+        return [fileVerStr,strSpltLs[0],strSpltLs[1]]
+    else:
+        return [None,strSpltLs[0],""]
+
     
 def filePathDestruction(filePath):
+    import os
     fileDir=os.path.dirname(filePath)
     fileBaseName=os.path.basename(filePath)
     getFileNameSplit=fileBaseName.rsplit(".",1)
     fileSourcName=getFileNameSplit[0]
     fileExt=getFileNameSplit[1]
-    fileVer=int(getFileVersionStr(fileSourcName))
-    print "fileDir: "+fileDir
-    print "fileBaseName: "+fileBaseName
-    print "fileSourcName: "+fileSourcName
-    print "fileExt: "+fileExt
-    print "fileVer: "+fileVer
+    fileVer=getFileVersionStr(fileSourcName)[0]
+    verPreStr= getFileVersionStr(fileSourcName)[1]
+    verPstStr= getFileVersionStr(fileSourcName)[2]
+    return [fileDir,fileBaseName,fileSourceName,fileExt,fileVer,verPreStr,verPstStr]
 
     
 filePathDestruction(testFile)
