6ed6288778ff7eeadf07eb787b2dfd17f8c1f025
jjstranger <jjstranger@qq.com>
2020-02-13 01:03:19 +0800

    refine fileProcs

--- T-Task/maya/scripts/working/trv_fileProcessing.py
+++ T-Task/maya/scripts/working/trv_fileProcessing.py
@@ -1,36 +1,36 @@
 testFile="C:/PRJ/dev/3001_JJS_PrjTst/VFX/sequences/SC001/SH_001/CG/scenes/Effects/abcOpSet_Test_jjs_v001.mb"
+import os,re
 
-def getFileVersionStr(fileSourcName):
-    verRuleStr="[\._][vV]\d+"
-    import re
+verRuleStr="[\._][vV]\d+[\._]*"
+def getFileVersionStr(fileSourceName,verRuleStr="[\._][vV]\d+[\._]*"):
     strSpltLs=re.split(verRuleStr,fileSourceName)
     if len(strSpltLs)==2:
-        fileVerStr=re.search(verRuleStr,fileSourcName).group()[2:]
+        fileVerStr=re.search(verRuleStr,fileSourceName).group()[2:]
         return [fileVerStr,strSpltLs[0],strSpltLs[1]]
     else:
         return [None,strSpltLs[0],""]
 
-    
-def filePathDestruction(filePath):
-    import os
+def filePathDestruction(filePath,verRuleStr="[\._][vV]\d+[\._]*"):
     fileDir=os.path.dirname(filePath)
     fileBaseName=os.path.basename(filePath)
     getFileNameSplit=fileBaseName.rsplit(".",1)
-    fileSourcName=getFileNameSplit[0]
+    fileSourceName=getFileNameSplit[0]
     fileExt=getFileNameSplit[1]
-    fileVer=getFileVersionStr(fileSourcName)[0]
-    verPreStr= getFileVersionStr(fileSourcName)[1]
-    verPstStr= getFileVersionStr(fileSourcName)[2]
+    getVerStrLs=getFileVersionStr(fileSourceName,verRuleStr)
+    fileVer=getVerStrLs[0]
+    verPreStr= getVerStrLs[1]
+    verPstStr= getVerStrLs[2]
     return [fileDir,fileBaseName,fileSourceName,fileExt,fileVer,verPreStr,verPstStr]
 
-    
-def raiseVerNum(filePath):
+def raiseVerNum(filePath,verRuleStr="[\._][vV]\d+[\._]*"):
     getFileInfs= filePathDestruction(filePath)
     getFileDir=getFileInfs[0]
-    fileRule=getFileInfs[5]+"[\._][vV]\d+[\._]*"+getFileInfs[6]+"."+getFileInfs[3]
+    fileRule=getFileInfs[5]+verRuleStr+getFileInfs[6]+"."+getFileInfs[3]
     fileLs=[f for f in os.listdir(getFileDir) if re.findall(fileRule,f)]
     verStrLs=[]
     for f in fileLs:
-         erStrLs.append(int(re.findall("[\._][vV]\d+[\._]*",f)[-1][2:-1]))
+         verStrLs.append(int(re.findall(verRuleStr,f)[-1][2:-1]))
     verNum=str(max(verStrLs)+1)
     return verNum.zfill(3)
+
+print raiseVerNum(testFile,verRuleStr)
